fe830f26af53ea1efe3c742b32e758879a08e5eb
NicoMaker <nicola.marano02gmail,com>
2025-01-21 16:39:00 +0100

    ottimizazion css

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,14 +1,25 @@
-export function Card({pokemon}) {
-
-    return (
-            <div className="card" style={{width: 18 + 'rem'}}>
-                <img src={pokemon.sprites.front_default} className="card-img-top" alt={pokemon.name}/>
-                <div className="card-body">
-                    <h5 className="card-title">{pokemon.name}</h5>
-                    <p className="card-text">
-                        {pokemon.types.map(t => <button key={t.slot} type="button" className=" button btn btn-warning">{t.type.name}</button> )}
-                    </p>
-                </div>
-            </div>
-    )
+export function Card({ pokemon }) {
+  return (
+    <div className="card" style={{ width: 18 + "rem" }}>
+      <img
+        src={pokemon.sprites.front_default}
+        className="card-img-top"
+        alt={pokemon.name}
+      />
+      <div className="card-body">
+        <h5 className="card-title">{pokemon.name}</h5>
+        <p className="card-text">
+          {pokemon.types.map((t) => (
+            <button
+              key={t.slot}
+              type="button"
+              className=" button btn btn-warning"
+            >
+              {t.type.name}
+            </button>
+          ))}
+        </p>
+      </div>
+    </div>
+  );
 }
